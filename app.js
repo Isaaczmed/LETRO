2af066d6c53b8945593b9778f3e3e54b0ed487fc
Isaaczmed <zanibonisaac@gmail.com>
2024-11-01 23:33:32 -0300

    Comentários e conclusões de algumas funções

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 function selecionarBloco(num, lin) {
     let linhaSelecionada = document.getElementById("linha" + lin);
-    if (linhaSelecionada.classList.contains("linha_selecionada")){
-
+    if (linhaSelecionada.classList.contains("linha_selecionada")) {
         let blocoSelecionado = document.getElementById("bloco" + num);
         let blocosNaLinha = linhaSelecionada.getElementsByClassName("bloco");
-    
+
+        // Passa por todos os blocos da linha para ver se algum já estava selecionado
         for (let i = 0; i < blocosNaLinha.length; i++) {
             if (blocosNaLinha[i].classList.contains("bloco_selecionado")) {
-    
                 blocosNaLinha[i].classList.remove("bloco_selecionado");
             }
         }
@@ -15,15 +14,33 @@ function selecionarBloco(num, lin) {
     }
 }
 
-function apenasLetras(){
+function apenasLetras() {
     const inputs = document.querySelectorAll('.letra');
 
+    // Não permite enviar algo além de letras no input
     inputs.forEach(input => {
         input.addEventListener('keypress', function (e) {
-            // Permite apenas letras (a-z, A-Z) e impede números
             if (!/^[a-zA-Z]$/.test(e.key)) {
                 e.preventDefault();
             }
         });
+
+        // Caso não seja vazio e seja um número muda a cor da letra
+        input.addEventListener('input', function () {
+            if (!isNaN(this.value) && this.value.length > 0) {
+                this.style.backgroundColor = '#5C4D8B'; 
+                this.style.color = '#5C4D8B'; 
+            } else {
+                this.style.backgroundColor = 'transparent'; 
+                this.style.color = '#FFFFFF'; 
+            }
+        });
+
+        // O input selecionado ganha foco
+        input.addEventListener('focus', function () {
+            this.setSelectionRange(0, 1); 
+        });
     });
 }
+
+apenasLetras();
